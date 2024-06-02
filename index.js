d6731c86be9693cb570bc710887a95a5d5695f7e
danylov-listex <69152892+danylov-listex@users.noreply.github.com>
2024-06-02 17:04:39 +0300

    2 add / router

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 const PORT = 5555;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+router.get("/", (req, res) => {
+    res.send("App is running..");
 });
